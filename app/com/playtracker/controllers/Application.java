dd74a8fab288e798d459db0db8027e9f9b9d549d
malachite-- <ioana.sebestyen@yahoo.com>
2012-10-09 22:36:28 +0300

    Added project structure.

---
+++ app/com/playtracker/controllers/Application.java
@@ -0,0 +1,13 @@
+package com.playtracker.controllers;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+
+import com.playtracker.views.html.test;
+
+public class Application extends Controller {
+
+	public static Result test() {
+		return ok(test.render("This is a test."));
+	}
+}
