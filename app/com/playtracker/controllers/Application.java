0814ecd601df8e959aa95fc150af92ae2dcdd8bd
Ioana Sebestyen <ioana.sebestyen@yahoo.com>
2012-10-12 16:07:59 +0300

    Added db settings. Added User entity.

--- app/com/playtracker/controllers/Application.java
+++ app/com/playtracker/controllers/Application.java
@@ -1,14 +1,19 @@
 package com.playtracker.controllers;
 
+import play.db.jpa.JPA;
+import play.db.jpa.Transactional;
 import play.mvc.Controller;
 import play.mvc.Result;
 
+import com.playtracker.model.User;
 import com.playtracker.views.html.test;
 
 public class Application extends Controller {
 
+	@Transactional
 	public static Result test() {
-		return ok(test.render("This is a test."));
+		User u = JPA.em().find(User.class, 1L);
+		return ok(test.render(u == null ? "MIMI" : u.getUsername()));
 	}
 
 	public static Result test2() {
